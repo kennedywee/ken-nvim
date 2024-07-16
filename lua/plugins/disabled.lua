f73c33e98db23b988f46febbdfa10d3b86289cb0
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-07-16 08:57:35 +0800

    disable bufferline and for vuejs

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/which-key.nvim",
+    enabled = false,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,26 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = { "RRethy/nvim-treesitter-endwise" },
     opts = function(_, opts)
-      opts.endwise = { enabled = true }
-      opts.indent = { enabled = true, disabled = { "yaml", "ruby" } }
+      opts.endwise = { enable = true }
+      opts.indent = { enable = true, disable = { "yaml", "ruby" } }
+      opts.ensure_installed = {
+        "bash",
+        "embedded_template",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      }
     end,
   },
 }
